0001551617fe93de1b175c2bb9f69b0d22846aac
rmeirosu <rmeirosu@deloittece.com>
2023-08-22 14:55:33 +0300

    Initial commit for Rest Assured

---
+++ src/test/java/com/rest/Test.java
@@ -0,0 +1,17 @@
+package com.rest;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class Test {
+
+    @org.testng.annotations.Test
+
+    public void test() {
+        given()
+        .when()
+        .then();
+
+    }
+}
